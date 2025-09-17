2b44d6698f5778b781f4d99e85f0d0752b2698de
Ricardo <tu-id@users.noreply.github.com>
2025-09-16 23:37:43 -0300

    Inicio del proyecto

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+
+<div class="imagen-con-logo">
+  <img src="/imagen.jpg" alt="Fondo" class="img-fluid fullscreen-img" />
+  <img src="/vite.svg" alt="Logo" class="logo" />
+</div>
+`;

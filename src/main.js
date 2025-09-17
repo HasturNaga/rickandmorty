7f60d509abb706c89e8e32ee51f20bbf2600c516
Ricardo <tu-id@users.noreply.github.com>
2025-09-17 11:57:01 -0300

    agregando-titulo

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ document.querySelector('#app').innerHTML = `
 <div class="imagen-con-logo">
   <img src="/imagen.jpg" alt="Fondo" class="img-fluid fullscreen-img" />
   <img src="/vite.svg" alt="Logo" class="logo" />
+  <h1 class="titulo">Bienvenido a la API de Rick y Morty</h1>
 </div>
+
 `;

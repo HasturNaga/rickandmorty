3969dc96756fd8543b3cd180c750c30667c53f16
Ricardo <tu-id@users.noreply.github.com>
2025-09-18 16:02:14 -0300

    FronEnd terminado

--- src/main.js
+++ src/main.js
@@ -2,15 +2,33 @@ import './style.css';
 
 document.querySelector('#app').innerHTML = `
 
-<div class="imagen-con-logo">
-  <img src="/imagen.jpg" alt="Fondo" class="img-fluid fullscreen-img" />
-  <img src="/vite.svg" alt="Logo" class="logo" />
-  <h1 class="titulo">Bienvenido a la API de Rick y Morty</h1>
+<div class="hero d-flex">
+  <!-- Contenido superpuesto -->
+  <div class="contenido text-white">
+    <img src="/vite.svg" alt="Logo" class="mb-3" style="min-width: 5vw;">
+    <h1 class="titulo mb-4">Bienvenido a la API de Rick y Morty</h1>
+
+    <form class="mb-3">
+      <div class="input-group" style="max-width: 400px;">
+        <input type="text" class="form-control" placeholder="Buscar personaje..." id="inputValue">
+        <button class="btn btn-primary" type="button" id="buscarPorId">Buscar por nombre</button>
+      </div>
+    </form>
+
+    <div class="d-flex flex-wrap mb-3" style="max-width: 400px;">
+      <select id="ubicaciones" class="form-select m-2 flex-grow-1">
+        <option value="" selected>Selecciona una ubicación</option>
+      </select>
+      <button class="btn btn-primary m-2" type="button" id="buscarPorUbicacion">Buscar por ubicación</button>
+    </div>
+
+    <div id="paginaction" class="d-flex flex-wrap align-items-center"></div>
+    <div id="resultado" class="d-flex flex-wrap"></div>
+  </div>
 </div>
 
-
-
-<footer class="bg-dark text-light py3 text-center>
-  <p>Todos los derechos reservados</p>
+<!-- Footer -->
+<footer class="bg-dark text-light py-3 text-center">
+  <p class="m-0">Todos los derechos reservados <span>2023</span></p>
 </footer>
 `;
